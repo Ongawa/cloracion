81682ede523d4ad81168c746f8f5c6d2c9acfd7e
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2015-11-29 20:49:21 +0100

    Add all elements to the cl calculation view

--- src/main/java/org/ongawa/peru/chlorination/gui/manage/ChlorinationWindow.java
+++ src/main/java/org/ongawa/peru/chlorination/gui/manage/ChlorinationWindow.java
@@ -2,4 +2,16 @@ package org.ongawa.peru.chlorination.gui.manage;
 
 public class ChlorinationWindow {
 
+    
+    public void triggerCalculation(){
+        // TODO: Do the calculations
+    }
+    
+    public void triggerSave() {
+        // TODO: Save the results
+    }
+    
+    public void triggerPrint() {
+        // TODO: Print the results
+    }
 }
