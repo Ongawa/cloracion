79941e168dd907f2f44159b0a3b9ff81fce6be03
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2016-02-10 22:36:29 +0100

    Make application properties local file, auto-recreated if missing

--- src/main/java/org/ongawa/peru/chlorination/ApplicationProperties.java
+++ src/main/java/org/ongawa/peru/chlorination/ApplicationProperties.java
@@ -1,12 +1,17 @@
 package org.ongawa.peru.chlorination;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Properties;
+
+import com.itextpdf.text.log.SysoCounter;
 
 /**
  * @author Kiko
@@ -32,10 +37,28 @@ public class ApplicationProperties {
 	
 	private void readProperties() throws IOException{
 		this.properties = new Properties();
-		String resourceName = KEYS.PROPERTIES_FILENAME;
+		/*String resourceName = KEYS.PROPERTIES_FILENAME;
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		InputStream resourceStream = loader.getResourceAsStream(resourceName);
-		this.properties.load(resourceStream);
+		InputStream resourceStream = loader.getResourceAsStream(resourceName);*/
+		InputStream bundledPropertiesFile = ApplicationProperties.class.getResourceAsStream("/" + KEYS.PROPERTIES_FILENAME);
+		Properties bundledProperties = new Properties();
+		bundledProperties.load(bundledPropertiesFile);
+		
+		// If the local properties does not exists, create it:
+		// Remember to replace the '~' if present
+		File localDataFolder = new File(bundledProperties.getProperty(KEYS.LOCAL_DATA_FOLDER).replaceFirst("~",
+		                                System.getProperty("user.home")));
+		File localPropertiesFile = new File( localDataFolder.getAbsoluteFile(), KEYS.PROPERTIES_FILENAME);
+		
+		// Absolute path for files:
+		if (! localPropertiesFile.exists()) {
+		    localDataFolder.getAbsoluteFile().mkdir();
+		    localPropertiesFile.createNewFile();
+		    bundledProperties.store(new FileOutputStream(localPropertiesFile),"Initial properties for Chlorination app");
+		}
+		
+		
+		this.properties.load(new FileInputStream(localPropertiesFile));
 	}
 	
 	public Properties getProperties(){
@@ -43,8 +66,11 @@ public class ApplicationProperties {
 	}
 	
 	public void storeProperties() throws FileNotFoundException, IOException{
-		Path path  = FileSystems.getDefault().getPath(properties.getProperty(KEYS.RESOURCES_PATH), KEYS.PROPERTIES_FILENAME);
-		this.properties.store(new FileOutputStream(path.toFile()), "Generated properties file for Chlorination app");
+	    // Remember to replace the '~' if present
+	    File localDataFolder = new File(this.properties.getProperty(KEYS.LOCAL_DATA_FOLDER).replaceFirst("~",
+                System.getProperty("user.home")));
+	    File localPropertiesFile = new File(localDataFolder, KEYS.PROPERTIES_FILENAME);
+	    this.properties.store(new FileOutputStream(localPropertiesFile), "Generated properties file for Chlorination app");
 		this.readProperties();
 	}
 }
--- src/main/java/org/ongawa/peru/chlorination/KEYS.java
+++ src/main/java/org/ongawa/peru/chlorination/KEYS.java
@@ -6,6 +6,8 @@ package org.ongawa.peru.chlorination;
 public class KEYS {
 	public static final String PROPERTIES_FILENAME = "application.properties";
 	
+	public static final String LOCAL_DATA_FOLDER="LOCAL_DATA_FOLDER";
+	
 	public static final String DATABASE_URL = "DATABASE_URL";
 	public static final String DATABASE_USERNAME = "DATABASE_USERNAME";
 	public static final String DATABASE_PASSWORD = "DATABASE_PASSWORD";
